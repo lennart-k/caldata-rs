ca30deee4a39f13b7f2515d74d38ce1694e9ee7d
Lennart Kämmle <18233294+lennart-k@users.noreply.github.com>
2026-06-29 19:43:42 +0200

    Add unit test for dtstart property

--- src/property/dtstart.rs
+++ src/property/dtstart.rs
@@ -10,7 +10,13 @@ super::property!(
 #[cfg(test)]
 mod tests {
     use super::IcalDTSTARTProperty;
-    use crate::{generator::Emitter, parser::ICalProperty, property::ContentLine};
+    use crate::{
+        generator::Emitter,
+        parser::{ContentLineParams, ICalProperty},
+        property::ContentLine,
+        types::{CalDateOrDateTime, Tz},
+    };
+    use chrono::{DateTime, TimeZone};
     use rstest::rstest;
     use std::collections::HashMap;
 
@@ -29,5 +35,28 @@ mod tests {
         let prop = IcalDTSTARTProperty::parse_prop(&content_line, Some(&timezones)).unwrap();
         let roundtrip: ContentLine = prop.into();
         similar_asserts::assert_eq!(roundtrip.generate(), input);
+    }
+
+    #[rstest]
+    #[case(Tz::Olson(chrono_tz::Europe::Berlin)
+            .with_ymd_and_hms(2021, 1, 1, 0, 0, 0)
+            .unwrap(), "DTSTART;TZID=Europe/Berlin:20210101T000000\r\n")]
+    #[case(Tz::Local
+            .with_ymd_and_hms(2021, 1, 1, 0, 0, 0)
+            .unwrap(), "DTSTART:20210101T000000\r\n")]
+    #[case(Tz::Olson(chrono_tz::UTC)
+            .with_ymd_and_hms(2021, 1, 1, 0, 0, 0)
+            .unwrap(), "DTSTART:20210101T000000Z\r\n")]
+    fn test_tzid_content_line(#[case] input: DateTime<Tz>, #[case] expected: &'static str) {
+        let cal_datetime: CalDateOrDateTime = input.into();
+        let timezone = cal_datetime.timezone();
+        let mut params = vec![];
+        if let Some(tzid) = timezone.tzid() {
+            params.push(("TZID".to_owned(), vec![tzid.to_owned()]));
+        }
+        let prop: IcalDTSTARTProperty =
+            IcalDTSTARTProperty(cal_datetime, ContentLineParams(params));
+        let content_line: ContentLine = prop.into();
+        similar_asserts::assert_eq!(content_line.generate(), expected);
     }
 }
